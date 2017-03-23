c97ed01f5e9550e2ee54e7d357d2f7532d351e5e
Stina Markan <kontakt@stinamarkan.se>
2017-03-23 21:04:07 +0100

    Initial push

--- ProjectFiles/src/main/Main.java
+++ ProjectFiles/src/main/Main.java
@@ -1,14 +1,28 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	public static void main(String[] args) {
 
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 		
-	}
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+	    
+        Scene scene = new Scene(root, 1280, 720);
+    
+        primaryStage.setTitle("TO BE DECIDED");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
+}
 }

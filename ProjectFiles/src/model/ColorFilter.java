150b39ec00a3e6b43a7e620967d9d0d19134b619
Anton Hagvall <anthagv@student.chalmers.se>
2017-04-01 14:22:02 +0200

    Början på ColorFilter

--- ProjectFiles/src/model/ColorFilter.java
+++ ProjectFiles/src/model/ColorFilter.java
@@ -1,5 +1,20 @@
 package model;
 
+import javafx.scene.paint.Color;
+
 public class ColorFilter {
+
+    protected static LoadedImage transform(LoadedImage img, double r, double g, double b) {
+        LoadedImage newImage = new LoadedImage(img);
+
+        for(int i = 0; i < newImage.pxImage.length; i++){
+            for(int j = 0; j < newImage.pxImage[i].length; j++){
+
+                Color pxColor = newImage.pxImage[i][j];
+                pxColor = Color.rgb((int) (pxColor.getRed() * r), (int) (pxColor.getGreen() * g), (int) (pxColor.getBlue() * b));
+            }
+        }
+        return newImage;
+    }
 
 }

85ff905230ec172767e56e09fb28f489e033b2cc
stinamarkan <stina895@hotmail.com>
2017-04-04 11:08:28 +0200

    Adding fade transition in toolbar + code clean up

--- ProjectFiles/src/controllers/ToolView.java
+++ ProjectFiles/src/controllers/ToolView.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class ToolView extends AnchorPane implements Initializable {
 	
@@ -27,7 +30,6 @@ public class ToolView extends AnchorPane implements Initializable {
 	
 	public ToolView() {
 
-		
 		FXMLLoader fxmlLoader =	new FXMLLoader(getClass().getResource("/resources/fxml/ToolView.fxml"));
 		System.out.println("toolview");
 		fxmlLoader.setRoot(this);
@@ -37,45 +39,64 @@ public class ToolView extends AnchorPane implements Initializable {
 			fxmlLoader.load();
 		} catch (IOException exception) {
 			throw new RuntimeException(exception);
-		}
-		
+		}	
 	}
 
+	private FadeTransition fadeIn = new FadeTransition(Duration.millis(150));
+	private FadeTransition fadeAdjust = new FadeTransition(Duration.millis(150));
+	private FadeTransition fadeColor = new FadeTransition(Duration.millis(150));
+	private FadeTransition fadeEffect = new FadeTransition(Duration.millis(150));
+	private FadeTransition fadeFilter = new FadeTransition(Duration.millis(150));
+	
+	private void fadeSettings(FadeTransition name, Node node){
+		name.setNode(node);
+		name.setFromValue(0.0);
+		name.setToValue(1.0);
+		name.setCycleCount(1);
+		name.setAutoReverse(false);
+		return;
+	}
+	
+	private void mouseClicked(Node begin, Node end, FadeTransition fade){
+		begin.setVisible(false);
+		end.setVisible(true);
+		fade.playFromStart();
+		return;
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("init tools");
 		
+		fadeSettings(fadeIn, topLevel);
+		fadeSettings(fadeAdjust, adjustLevel);
+		fadeSettings(fadeColor, colorLevel);
+		fadeSettings(fadeEffect, effectLevel);
+		fadeSettings(fadeFilter, filterLevel);
+
 		adjustIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(false);
-			adjustLevel.setVisible(true);
+			mouseClicked(topLevel, adjustLevel, fadeAdjust);
 		});
 		aBackIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(true);
-			adjustLevel.setVisible(false);
+			mouseClicked(adjustLevel, topLevel, fadeIn);
 		});
 		colorIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(false);
-			colorLevel.setVisible(true);
+			mouseClicked(topLevel, colorLevel, fadeColor);
 		});
 		cBackIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(true);
-			colorLevel.setVisible(false);
+			mouseClicked(colorLevel, topLevel, fadeIn);
 		});
 		effectIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(false);
-			effectLevel.setVisible(true);
+			mouseClicked(topLevel, effectLevel, fadeEffect);
 		});	
 		eBackIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(true);
-			effectLevel.setVisible(false);
+			mouseClicked(effectLevel, topLevel, fadeIn);
 		});		
 		filterIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(false);
-			filterLevel.setVisible(true);
+			mouseClicked(topLevel, filterLevel, fadeFilter);
 		});	
 		fBackIcon.setOnMouseClicked(e -> {
-			topLevel.setVisible(true);
-			filterLevel.setVisible(false);
+			mouseClicked(filterLevel, topLevel, fadeIn);
 		});
 	}
 	

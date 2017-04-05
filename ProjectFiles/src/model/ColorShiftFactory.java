f8a113874e9371b7382d63f968698f98276bdeb1
Anton Hagvall <anthagv@student.chalmers.se>
2017-04-05 21:53:49 +0200

    deleted ShiftColor and ShiftColoFactory

--- ProjectFiles/src/model/ColorShiftFactory.java
+++ ProjectFiles/src/model/ColorShiftFactory.java
@@ -9,12 +9,19 @@ public class ColorShiftFactory {
         if(color == null) {
             return null;
         }
-        if(color == ColorShiftType.BLUE) {
-            return new ColorShift(, 0.85, 1.30);
+        if(color == ColorShiftType.RED) {
+            return new ColorShift(1.30, 0.85, 0.85);
         }
         else if(color == ColorShiftType.GREEN) {
-
+            return new ColorShift(0.85, 1.30, 0.85);
         }
+        else if(color == ColorShiftType.BLUE) {
+            return new ColorShift(0.85, 0.85, 1.30);
+        }
+
+        //Add more ColorShifts
+        //...
+        //...
 
         return null;
     }

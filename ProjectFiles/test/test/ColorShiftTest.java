b324dc829e5b6d86c63dc2763359ba4f01c2109b
Anton Hagvall <anthagv@student.chalmers.se>
2017-04-21 13:08:58 +0200

    testing

--- ProjectFiles/test/test/ColorShiftTest.java
+++ ProjectFiles/test/test/ColorShiftTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import javafx.scene.paint.Color;
 import model.LoadedImage;
 import org.junit.Test;
 
@@ -12,10 +13,14 @@ import static org.junit.Assert.*;
  */
 public class ColorShiftTest {
     @Test
-    public void transform() throws Exception {
+    public void transformTest() throws Exception {
         BufferedImage testImage = new BufferedImage(2, 2, BufferedImage.TYPE_INT_ARGB);
         LoadedImage loadedImage = new LoadedImage(testImage);
-
+        loadedImage.pxImage[0][0] = Color.rgb(100, 0, 0);
+        loadedImage.pxImage[0][1] = Color.rgb(100, 100, 100);
+        loadedImage.pxImage[1][0] = Color.rgb(0, 100, 200);
+        loadedImage.pxImage[1][1] = Color.rgb(200, 100, 0);
+        System.out.println((int) (loadedImage.pxImage[0][0].getRed() * 255));
 
     }
 

acd8f0a1f13d7228cd2067c43f4ca84d38de4c99
Anton Hagvall <anthagv@student.chalmers.se>
2017-04-27 12:54:47 +0200

    Changed ColorShiftFactory

--- ProjectFiles/src/model/ColorShiftFactory.java
+++ ProjectFiles/src/model/ColorShiftFactory.java
@@ -8,13 +8,13 @@ public class ColorShiftFactory {
             return null;
         }
         if(color == ColorShiftType.RED) {
-            return new ColorShift(1.30, 0.85, 0.85);
+            return new ColorShift(50, -25, -25);
         }
         else if(color == ColorShiftType.GREEN) {
-            return new ColorShift(0.85, 1.30, 0.85);
+            return new ColorShift(-25, 50, -25);
         }
         else if(color == ColorShiftType.BLUE) {
-            return new ColorShift(0.85, 0.85, 1.30);
+            return new ColorShift(-25, -25, 50);
         }
 
         //Add more ColorShifts

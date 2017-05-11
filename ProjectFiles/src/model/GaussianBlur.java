93a7b6a1c65ada5697580008d82145b337db1ff1
nicket97 <niclas.o.johansson@telia.com>
2017-05-11 11:07:39 +0200

    fixing tha sharpen and gausian logic

--- ProjectFiles/src/model/GaussianBlur.java
+++ ProjectFiles/src/model/GaussianBlur.java
@@ -44,15 +44,20 @@ LoadedImage newImage = new LoadedImage(img);
 				int sumGreen = 0;
 				int sumBlue = 0;
 				int count = 0;
+				int x = 0;
 				for(int k = -1*radius; k < radius; k++) {
+					int y = 0;
 					for (int l = -1*radius; l < radius; l++) {
 						if((i+k) >= 0 && (j+l) >= 0 && (i+k) < img.getpxImage().length && (j+l) < img.getpxImage()[i].length ){
-						sumRed += img.getpxImage()[i+k][j+l].getRed()*255;
-						sumGreen += img.getpxImage()[i+k][j+l].getGreen()*255;
-						sumBlue += img.getpxImage()[i+k][j+l].getBlue()*255;
-						count++;
+						sumRed += img.getpxImage()[i+k][j+l].getRed()*255*kernel[x][y];
+						sumGreen += img.getpxImage()[i+k][j+l].getGreen()*255*kernel[x][y];
+						sumBlue += img.getpxImage()[i+k][j+l].getBlue()*255*kernel[x][y];
+						count += kernel[x][y];
+						y++;
 						}
+						
 					}
+					x++;
 				}
 				newImage.getpxImage()[i][j]= Color.rgb(sumRed/count, sumGreen / count, sumBlue/count);
 				}
--- ProjectFiles/src/model/Sharpen.java
+++ ProjectFiles/src/model/Sharpen.java
@@ -14,13 +14,13 @@ public class Sharpen implements Layerable {
 		
 		 kernel = new double[3][3];
 		 kernel[0][0] = 0;
-		 kernel[0][1] = -2;
+		 kernel[0][1] = -1;
 		 kernel[0][2] = 0;
-		 kernel[1][0] = -2;
-		 kernel[1][1] = 10;
-		 kernel[1][2] = -2;
+		 kernel[1][0] = -1;
+		 kernel[1][1] = 5;
+		 kernel[1][2] = -1;
 		 kernel[2][0] = 0;
-		 kernel[2][1] = -2;
+		 kernel[2][1] = -1;
 		 kernel[2][2] = 0;
 	
 		
@@ -31,23 +31,37 @@ public class Sharpen implements Layerable {
 LoadedImage newImage = new LoadedImage(img);
 		radius = 1;
 		
-		for(int i = 0; i < img.getpxImage().length; i++) {
-			for(int j = 0; j < img.getpxImage()[i].length; j++) {
+		for(int i = 1; i < img.getpxImage().length-1; i++) {
+			for(int j = 1; j < img.getpxImage()[i].length-1; j++) {
 				int sumRed = 0;
 				int sumGreen = 0;
 				int sumBlue = 0;
 				int count = 0;
-				for(int k = -1*radius; k < radius; k++) {
-					for (int l = -1*radius; l < radius; l++) {
+				int x = 0;
+				for(int k = -1*radius; k <= radius; k++) {
+					int y = 0;
+					for (int l = -1*radius; l <= radius; l++) {
 						if((i+k) >= 0 && (j+l) >= 0 && (i+k) < img.getpxImage().length && (j+l) < img.getpxImage()[i].length ){
-						sumRed += img.getpxImage()[i+k][j+l].getRed()*255;
-						sumGreen += img.getpxImage()[i+k][j+l].getGreen()*255;
-						sumBlue += img.getpxImage()[i+k][j+l].getBlue()*255;
+						sumRed += img.getpxImage()[i+k][j+l].getRed()*254*kernel[x][y];
+						sumGreen += img.getpxImage()[i+k][j+l].getGreen()*254*kernel[x][y];
+						sumBlue += img.getpxImage()[i+k][j+l].getBlue()*254*kernel[x][y];
 						count++;
+						y++;
 						}
+						else{
+							
+						}
+						
 					}
+					x++;
 				}
-				newImage.getpxImage()[i][j]= Color.rgb(sumRed/count, sumGreen / count, sumBlue/count);
+				if(sumRed < 0) sumRed = 0;
+				if(sumRed > 255) sumRed = 255;
+				if(sumGreen < 0) sumGreen = 0;
+				if(sumGreen > 255) sumGreen = 255;
+				if(sumBlue < 0) sumBlue = 0;
+				if(sumBlue > 255) sumBlue = 255;
+				newImage.getpxImage()[i][j]= Color.rgb(sumRed, sumGreen, sumBlue);
 				}
 		}
 		return newImage;

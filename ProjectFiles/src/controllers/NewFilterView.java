f4c0f37769e9bdd5159267fb05295ab7322dbb68
hannajacobsson <hanna-jacobsson@hotmail.com>
2017-05-16 18:05:10 +0200

    new filter function

--- ProjectFiles/src/controllers/NewFilterView.java
+++ ProjectFiles/src/controllers/NewFilterView.java
@@ -4,11 +4,23 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import model.Layer;
 
 public class NewFilterView extends AnchorPane implements Initializable {
+	
+	@FXML AnchorPane canvasPane;
+	@FXML TextField grid00, grid01, grid02, grid10, grid11, grid12, grid20, grid21, grid22, nameInput;
+	@FXML Button newFilterCancelButton, newFilterSaveButton;
+	
+	
 	
 	
 	public NewFilterView() {
@@ -16,20 +28,40 @@ public class NewFilterView extends AnchorPane implements Initializable {
 		System.out.println("new filter view");
 		fxmlLoader.setController(this);
 		fxmlLoader.setRoot(this);
-		
-		try {
-			fxmlLoader.load();
-		} catch (IOException exception) {
-			throw new RuntimeException(exception);
-		}
 
-	}
+			try {
+				fxmlLoader.load();
+			} catch (IOException exception) {
+				throw new RuntimeException(exception);
+			
+		}
+		}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("init newFilter");
-
+		double[][] gridValues = new double[3][3];
+		newFilterSaveButton.setOnAction( e->{
+			gridValues[0][0] = Double.parseDouble(grid00.getText());
+			gridValues[0][1] = Double.parseDouble(grid01.getText());
+			gridValues[0][2] = Double.parseDouble(grid02.getText());
+			gridValues[1][0] = Double.parseDouble(grid10.getText());
+			gridValues[1][1] = Double.parseDouble(grid11.getText());
+			gridValues[1][2] = Double.parseDouble(grid12.getText());
+			gridValues[2][0] = Double.parseDouble(grid20.getText());
+			gridValues[2][1] = Double.parseDouble(grid21.getText());
+			gridValues[2][2] = Double.parseDouble(grid22.getText());
+			
+			for (int i = 0; i < gridValues.length; i++) {
+				for (int j = 0; j < gridValues[0].length;j++) {
+					System.out.println(gridValues[i][j]);
+				}
+			}
+		});
 		
 	}
+	
+
+	
 
 }
